2a42ac7d70690a8a12467154d28c0c634e10f093
XZENIOR <gloogl942@gmail.com>
2026-08-25 21:07:34 +0700

    add followersCount page

---
+++ followersCount/followers_count.js
@@ -0,0 +1,34 @@
+let count = 0;
+let booster = 0;
+
+let notified10 = false;
+let notified20 = false;
+
+function increaseCount() {
+    count = count + booster;
+    displayCount();
+    checkCountValue();
+}
+
+function upgrade() {
+    booster++;
+    displayFollowersPerC();
+}
+
+function displayCount() {
+    document.getElementById("countDisplay").innerHTML = count;
+}
+
+function displayFollowersPerC() {
+    document.getElementById("countFollowersPerC").innerHTML = booster;
+}
+
+function checkCountValue() {
+    if (!notified20 && count >= 20) {
+        alert("Ваш аккаунт набрал 20 подписчиков!");
+        notified20 = true;
+    } else if (!notified10 && count >= 10) {
+        alert("Ваш аккаунт набрал 10 подписчиков!");
+        notified10 = true;
+    }
+}
